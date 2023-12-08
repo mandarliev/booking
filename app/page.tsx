ab55fe740a92e6c04b5c54a125e084363492262f
Dimitar <dimitar.mandarliev@gmail.com>
2023-12-08 02:02:45 +0200

    Building the Search Functionality (Search Component)

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import SearchForm from "@/components/SearchForm";
 import { trending_data } from "@/data/trending";
 
 export default function Home() {
@@ -10,7 +11,7 @@ export default function Home() {
         </h3>
       </section>
       <section className="m-4 mt-0 -mb-14 px-2 lg:px-4">
-        {/* SearchForm */}
+        <SearchForm />
       </section>
 
       <section className="mx-auto max-w-7xl mt-10 p-6 bg-white rounded-t-lg">
